302b981e4c093d90c4c679bf1f96a7276508e33f
turner harris <turnerharris7798@yahoo.com>
2020-08-20 14:14:17 -0500

    finished todos on christmas.cs

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -6,9 +6,10 @@ namespace ChristmasWithTypes
         public string[] Presents;
         public string Santa { get; set; }
         public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        TreeHeight = null;
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day { sun, mon, tue, wed, thur, fri, sat };
 
     }
 }
